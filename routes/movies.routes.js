84736e7451aef91031da92fbdb771babd2c38a06
Alicia Kmak <alicia.kmak@gmail.com>
2021-12-19 22:46:11 +0100

    add iteration 5: new movie. Mulitple selector for cast not working

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,20 @@
 const router = require("express").Router();
+const Movie = require("../models/movie.model");
+const Celebrity = require("../models/celebrity.model");
 
-// all your routes here
+// iteration 6 new movie
+
+router.get("/movies/create", (req, res, next) => {
+  res.render("movies/new-movie");
+});
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.create({ title, genre, plot, cast })
+    .then(() => res.redirect("/movies"))
+    .catch((error) => {
+      console.log("Error while creating movie occurred", error);
+      res.redirect("/movies/create");
+    });
+});
 
 module.exports = router;
