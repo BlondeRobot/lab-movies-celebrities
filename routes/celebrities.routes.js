1091d9d3139793ed54e831af56b793dd4017f9b9
Alicia Kmak <alicia.kmak@gmail.com>
2021-12-19 21:35:09 +0100

    add iteration 3: new celebrities. Working OK

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
+const Celebrity = require("../models/celebrity.model");
 
-// all your routes here
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+router.post("/celebrities/create", (req, res, next) => {
+
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => res.redirect("/celebrities"))
+    .catch((error) => {
+        console.log("Error while creating celebrity occurred", error)
+        res.redirect('/celebrities/create')
+        });
+});
 
 module.exports = router;
